d29c19e8ce0d3fb7a6de3982cd53e53797b305e7
surendra <surendraredddyalamuru01@gmail.com>
2024-12-12 13:23:05 +0530

    tools

--- misc/main.tf
+++ misc/main.tf
@@ -1,12 +1,12 @@
 provider "vault" {
-  address         = "http://172.31.89.193.rdevopsb81.online:8200"
+  address         = "http://172.31.89.193:8200"
   token           = var.vault_token
   skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
-resource "vault_mount" "roboshop" {
+resource "vault_mount" "roboshop-engine" {
   path        = "roboshop"
   type        = "kv-v2"
   options = {
