fcbb86f6baeb821d812c0486e433595abf4b5fab
surendra <surendraredddyalamuru01@gmail.com>
2024-12-13 09:21:04 +0530

    tools

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ infra:
       terraform apply -auto-approve
 
 ansible:
-       ansible-playbook -i 172.31.89.193, -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${tool_name} -e vault_token=${vault_token} main.yml
+       ansible-playbook -i 172.31.88.71, -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${tool_name} -e vault_token=${vault_token} main.yml
--- misc/main.tf
+++ misc/main.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "http://172.31.89.193:8200"
+  address         = "http://172.31.88.71:8200"
   token           = var.vault_token
   skip_tls_verify = true
 }
@@ -27,27 +27,27 @@ resource "vault_generic_secret" "ssh" {
 EOT
 }
 
-resource "vault_mount" "roboshop-parameters" {
-  path        = "roboshop-parameters"
-  type        = "kv-v2"
-  options = {
-    version = "2"
-    type    = "kv-v2"
-  }
-  description = "This is an roboshop secrets"
-}
-
-resource "vault_generic_secret" "catalogue" {
-  path = "${vault_mount.roboshop-parameters.path}/catalogue"
-
-  data_json = <<EOT
-{
-  "MONGO": "true",
-  "MONGO_URL" : "mongodb://mongodb-dev.rdevopsb81.online:27017/catalogue",
-  "DB_TYPE": "mongo",
-  "APP_GIT_URL": "https://github.com/roboshop-devops-project-v3/catalogue",
-  "DB_HOST": "mongodb-dev.rdevopsb81.online",
-  "SCHEMA_FILE": "db/master-data.js"
-}
-EOT
-}
+#resource "vault_mount" "roboshop-parameters" {
+#  path        = "roboshop-parameters"
+#  type        = "kv-v2"
+#  options = {
+#    version = "2"
+#    type    = "kv-v2"
+#  }
+#  description = "This is an roboshop secrets"
+#}
+#
+#resource "vault_generic_secret" "catalogue" {
+#  path = "${vault_mount.roboshop-parameters.path}/catalogue"
+#
+#  data_json = <<EOT
+#{
+#  "MONGO": "true",
+#  "MONGO_URL" : "mongodb://mongodb-dev.rdevopsb81.online:27017/catalogue",
+#  "DB_TYPE": "mongo",
+#  "APP_GIT_URL": "https://github.com/roboshop-devops-project-v3/catalogue",
+#  "DB_HOST": "mongodb-dev.rdevopsb81.online",
+#  "SCHEMA_FILE": "db/master-data.js"
+#}
+#EOT
+#}
