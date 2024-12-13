979ffad41d8397ffbd612c6ec899e203d7e41fdc
surendra <surendraredddyalamuru01@gmail.com>
2024-12-13 09:31:38 +0530

    tools

--- ec2-module/provider.tf
+++ ec2-module/provider.tf
@@ -1,7 +1,7 @@
-provider "vault" {
-
-  address = "http://172.31.89.193:8200"
-  skip_tls_verify = true
-  token = "hvs.ZwBtxYgToxbSE4lplUc9XnKf"
-}
+#provider "vault" {
+#
+#  address = "http://172.31.89.193:8200"
+#  skip_tls_verify = true
+#  token = "hvs.ZwBtxYgToxbSE4lplUc9XnKf"
+#}
 
