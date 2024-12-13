b04cd7837183dff0c64759250f724153909e4ff6
surendra <surendraredddyalamuru01@gmail.com>
2024-12-13 09:03:36 +0530

    tools

--- ec2-module/data.tf
+++ ec2-module/data.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  owners = ["973714476881"]
-}
-
-data "vault_generic_secret" "ssh"{
-  path = "infra-secrets/ssh"
-}
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  owners = ["973714476881"]
+#}
+#
+#data "vault_generic_secret" "ssh"{
+#  path = "infra-secrets/ssh"
+#}
 
